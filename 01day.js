24efb4e27a8522a75303589d6f6e8e6eb3502a75
pritamawatade <pritamawatade5@gmail.com>
2024-07-13 20:04:32 +0530

    day one

---
+++ 01day.js
@@ -0,0 +1,35 @@
+//  ACtvity one
+var a = 10
+console.log(a)
+let b  = "Chai aur code"
+console.log(b)
+
+//  ACtvity second
+
+const c = true
+console.log(c)
+
+//  ACtvity Third
+let d = ["a","b","c"]
+let e = {
+    chai : "code",
+
+}
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+
+
+// Activity four
+let firstvalue = 10
+console.log(firstvalue)
+firstvalue  = 101
+console.log(firstvalue)
+
+// Activity five
+const one = "first val"
+console.log(one)
+one = "second"  // Type error occured 
+console.log(one)
