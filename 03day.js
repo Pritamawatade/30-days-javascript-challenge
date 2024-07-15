763e24e8d0d264c0a49d4e3e5617aaafe8e4255a
pritamawatade <pritamawatade5@gmail.com>
2024-07-15 21:43:39 +0530

    day 03 complete

--- 03day.js
+++ 03day.js
@@ -74,8 +74,8 @@ let year = 2016
 
 
 if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    console.log("Not a leap year")
-} else {
     console.log(" a leap year")
+} else {
+    console.log("Not a leap year")
 
 }
